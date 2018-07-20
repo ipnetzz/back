7d1532e1dd970cbdaafba995f7a534ce65d9fc45
zp2946062178 <2799712178@qq.com>
2018-07-21 00:47:40 +0800

    create

---
+++ src/main/java/com/ipnet/entity/PersonalUser.java
@@ -0,0 +1,9 @@
+package com.ipnet.entity;
+
+/**
+ * @Author:zhangping
+ * @Description:
+ * @CreateData: 2018/7/21 0:45
+ */
+public class PersonalUser {
+}
---
+++ src/main/java/com/ipnet/enums/Identity.java
@@ -0,0 +1,9 @@
+package com.ipnet.enums;
+
+/**
+ * @Author:zhangping
+ * @Description:
+ * @CreateData: 2018/7/21 0:47
+ */
+public class Identity {
+}
